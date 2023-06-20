18354fadebd3cb742801319a0eb9e5feeb773ac2
DUKSOOKIM <96ejrtn@naver.com>
2023-06-20 13:43:36 +0900

    6/20 페이징 이동 작업 끝, 회원가입 작업 중

---
+++ src/test/java/com/example/monthly/service/UserServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.monthly.service;
+
+import com.example.monthly.dto.UserDto;
+import com.example.monthly.mapper.UserMapper;
+import com.example.monthly.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@Transactional
+@Slf4j
+class UserServiceTest {
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp(){
+        userDto = new UserDto();
+        userDto.setUserNumber(1L);
+        userDto.setUserId("testId");
+        userDto.setUserPassword("1234");
+        userDto.setUserName("케인");
+        userDto.setUserPhoneNumber("010-1234-1234");
+        userDto.setUserGender("M");
+        userDto.setUserEmail("test@email.com");
+        userDto.setUserBirthday("1993-09-08");
+        userDto.setUserStatus(1);
+    }
+
+    @Test
+    @DisplayName("회원 가입 검사")
+    void register() {
+        doNothing().when(userMapper).insert(any(UserDto.class));
+
+        userService.register(userDto);
+
+        verify(userMapper, times(1)).insert(userDto);
+    }
+
+}
