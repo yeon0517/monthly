d513443996761f40c91b5d01a98be3d075ca6d68
DUKSOOKIM <96ejrtn@naver.com>
2023-07-04 10:15:13 +0900

    7/4 외부 로그인 api작업 중

--- src/main/java/com/example/monthly/controller/user/UserRestController.java
+++ src/main/java/com/example/monthly/controller/user/UserRestController.java
@@ -1,9 +1,11 @@
 package com.example.monthly.controller.user;
 
 import com.example.monthly.service.user.UserService;
+import com.example.monthly.vo.UserVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class UserRestController {
         return userService.checkId(userId);
     }
 
-
+    @PostMapping("/registerKakao")
+    public void registerKakao(@RequestBody UserVo userVo) {
+        // Kakao 로그인 정보를 받아서 UserVo에 맞게 데이터 설정
+        // userService를 사용하여 UserVo를 DB에 저장
+        userService.registerKakao(userVo);
+    }
 }
